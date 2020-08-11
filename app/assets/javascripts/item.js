3310df229a520ea903b513b17c29514b73228404
tomoaki1169 <0111tomoaki@gmail.com>
2020-08-11 14:51:31 +0900

    画像複数投稿５

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,5 @@
 $(function() {
-  console.log("aaaa")
+  // console.log("aaaa")
   // 画像用のinputを生成する関数
     const buildFileField = (num)=> {
       const html = `<div data-index="${num}" class="js-file_group">
