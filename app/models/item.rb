ae7e9af933895e99fb792af3d51fe2069955900f
tomoaki1169 <0111tomoaki@gmail.com>
2020-08-11 12:04:38 +0900

    画像複数投稿4

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,32 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+  
+
+
+  validates :name,presence: true,length: {maximum: 40}
+
+  validates :price,numericality: { greater_than:300,less_than: 9999999,message:"販売中は300以上9,999,999以内で入力してください"}
+  validates :introduction, presence: true,length:{maximum: 1000}
+
+
+
   enum sale_status:{
     "販売中":0,
     "売り切れ":1
   }
-  enum category:{
-    
+
+  enum size:{
+    "XXS以下":0,
+    "XS(SS)":1,
+    "S":2,
+    "M":3,
+    "L":4,
+    "XL(LL)":5,
+    "2XL(3L)":6,
+    "3XL(4L)":7,
+    "4XL(5L)以上":8,
+    "FREE SIZE":9
   }
   enum status:{
     "新品、未使用": 0,
@@ -17,6 +37,18 @@ class Item < ApplicationRecord
     "全体的に状態が悪い": 5
     }
     
+    enum derivery_fee:{
+      "送料込み(出品者負担)":0,
+      "着払い(購入者負担)":1
+    }
+
+    enum data:{
+      "1~2日で発送":0,
+      "2~3日で発送":1,
+      "4~7日で発送":2
+    }
+
+
 end
 
 # @item.update(deal: "urikire")
--- db/migrate/20200807073225_create_items.rb
+++ db/migrate/20200807073225_create_items.rb
@@ -1,18 +1,18 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name#, null:false
-      t.integer :price#, null:false
-      t.string :derivery_fee#, null:false
-      t.string :size#, null:false
-      t.string :status#, null:false
-      t.string :data#, null:false
+      t.string :name, null:false
+      t.integer :price, null:false
+      t.string :derivery_fee, null:false
+      t.string :size, null:false
+      t.string :status, null:false
+      t.string :data, null:false
       t.string :area#, null:false
-      t.string :introduction#, null:false
+      t.string :introduction, null:false
       t.string :sale_status
-      t.references :user#, null:false,foreign_key:true
-      t.references :brand#, null:false,foreign_key:true
-      t.references :category#, null:false,foreign_key:true
+      t.references :user, null:false,foreign_key:true
+      t.references :brand, null:false,foreign_key:true
+      t.references :category, null:false,foreign_key:true
       t.timestamps
     end
   end
