b0e43bdef20f97cb105d6afb42df1425b8a37830
kosukejinnouchi <67685531+kosukejinnouchi@users.noreply.github.com>
2020-08-15 21:54:26 +0900

    Add-delete-function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:images).order('created_at DESC')
