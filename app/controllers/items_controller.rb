92a8d111bf2a5a980121232a7f4f0f14c23437cd
kosukejinnouchi <67685531+kosukejinnouchi@users.noreply.github.com>
2020-08-15 21:54:39 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,12 +34,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :edit
-    end
-
+    item = Item.find(params[:id])
+    item.destroy
   end
 
   private
