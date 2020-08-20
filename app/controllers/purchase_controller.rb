680f9b1b769af9cf2ff508d57e8dcc627fd49873
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-20 16:24:41 +0900

    delete.contoroller

--- app/controllers/purchase_controller.rb
+++
@@ -1,9 +0,0 @@
-class PurchaseController < ApplicationController
-  def index
-  end
-
-  def done
-    @item = Item.find(params[:id])
-    @item.update( purchase_id: current_user.id)
-  end
-end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,9 @@ class PurchasesController < ApplicationController
   layout "purchase"
   def index
   end
+
+  def done
+    @item = Item.find(params[:id])
+    @item.update( purchase_id: current_user.id)
+  end
 end
