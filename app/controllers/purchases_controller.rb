39fee27bb775f0b7ca6dffb1f74652c93a6a66d4
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-23 21:35:43 +0900

    Merge branch 'master' of https://github.com/tomoaki1169/furima_app2 into add-purchase-function

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,33 +3,34 @@ class PurchasesController < ApplicationController
   before_action :set_card, :set_item
 
   def index
-    if user_signed_in?
-      if current_user.credit_card.present?
-        Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-        customer = Payjp::Customer.retrieve(@credit_card.customer_id)
-        @customer_card = customer.cards.retrieve(@credit_card.card_id)
-        @card_brand = @customer_card.brand
-        case @card_brand
-        when "Visa"
-          @card_src = "visa.gif"
-        when "JCB"
-          @card_src = "jcb.gif"
-        when "MasterCard"
-          @card_src = "master.png"
-        when "American Express"
-          @card_src = "amex.gif"
-        when "Diners Club"
-          @card_src = "diners.gif"
-        when "Discover"
-          @card_src = "discover.gif"
-        end
-        @exp_month = @default_card_information.exp_month.to_s
-        @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
-      else
-      end
-    else
+    unless user_signed_in?
       redirect_to user_session_path, alert: "ログインしてください"
     end
+    if @credit_card.blank?
+      redirect_to 
+    else
+      current_user.credit_card.present?
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
+      customer = Payjp::Customer.retrieve(@credit_card.customer_id)
+      @customer_card = customer.cards.retrieve(@credit_card.card_id)
+      @card_brand = @customer_card.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.gif"
+      when "JCB"
+        @card_src = "jcb.gif"
+      when "MasterCard"
+        @card_src = "master.png"
+      when "American Express"
+        @card_src = "amex.gif"
+      when "Diners Club"
+        @card_src = "diners.gif"
+      when "Discover"
+        @card_src = "discover.gif"
+      end
+        @exp_month = @default_card_information.exp_month.to_s
+        @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
+      end
   end
 
   def pay
@@ -41,7 +42,7 @@ class PurchasesController < ApplicationController
           Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
           charge = payjp::Charge.create(
             amount: @item.price,
-            customer: Payjp::Customer,retrieve(@credit_card.customer_id),
+            customer: Payjp::Customer.retrieve(@credit_card.customer_id),
             currency: "jpy"
           )
           redirect_to action: 'done'
@@ -54,20 +55,17 @@ class PurchasesController < ApplicationController
   end
 
   layout "purchase"
-  def index
-  end
-
   def done
     @item.update( purchase_id: current_user.id)
   end
 
   private
   def set_card
-    @credit_card = CreditCard.find_by(user_id: current_user.index)
+    @credit_card = CreditCard.find_by(user_id: current_user.id)
   end
 
   def set_item
-    @item = Item.find(params[:item_id])
+    @items = Item.find(params[:item_id])
     @images = @item.images.all
   end
 end
