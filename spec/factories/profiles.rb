398c52b5ea3058246d53bffd2663e2cf0baf6927
gayabozu5963 <forestone.5963@gmail.com>
2020-08-23 19:59:54 +0900

    rspec-user-model

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :profile do
-    
+    family_name                 {"田中"}
+    first_name                  {"太朗"}
+    family_name_kana            {"タナカ"} 
+    first_name_kana             {"タロウ"}
+    birth_day                   {"2000-01-01"}
   end
 end
