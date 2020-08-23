69e07bd4e6871339cba82a873b538521570c2366
tomoaki1169 <0111tomoaki@gmail.com>
2020-08-23 22:05:33 +0900

    単体テスト１

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
-    
+    name {"baketu"}
+    price {900}
+    derivery_fee {0}
+    size {0}
+    data {0}
+    introduction {"aaa"}
+    status {0}
+    category_id {1}
+    prefecture_id {0}
+    user_id {1}
+    association :category
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name {"tomoaki"}
+    email {"tomoaki@tomoaki.com"}
+    encrypted_password {"tomoaki1169"}
   end
 end
