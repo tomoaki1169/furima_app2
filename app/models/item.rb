d81f2d971a3bbf6e4c38c25fb71c7b32597d499a
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-25 18:43:01 +0900

    create.model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
+  has_many :favorites
   has_one :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
