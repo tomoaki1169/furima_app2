c66cb0ec19a859d771e0e632d2a0e1f53564a55b
Komesu09 <67633415+Komesu09@users.noreply.github.com>
2020-08-27 12:27:04 +0900

    edit.controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,13 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_item
   
   def create
-    item = Item.find(params[:item_id])
-    favorite = Favorite.create(user_id: current_user.id, item_id: item.id)
+    @favorite = Favorite.create(user_id: current_user.id, item_id: @item.id)
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    favorite = Favorite.find_by(user_id: current_user.id, item_id: item.id)
-    favorite.destroy
+    @favorite = current_user.favorites.find_by(item_id: @item.id)
+    @favorite.destroy
   end
   
   private
